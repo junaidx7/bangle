5f6d014a0bdec528964e4cc2f696f61b22b71a11
Kushan Joshi <0o3ko0@gmail.com>
2023-07-30 23:31:02 -0400

    Chore/fix extension tests (#446)
    * refactor api internal
    
    * improve nsm editor manager
    
    * refactor editor sschema
    
    * add @bangle.io/bangle-store (and 5 others), remove @bangle.io/setup-store, update @testing-library/dom to ^8.20.1 (and 36 others)
    
    Commit generated via `yarn stage`
    
    * add test for seach ext
    
    * Commit generated via `yarn stage`
    
    * fix more ext tests
    
    * create @bangle.io/day-js, add @bangle.io/base-error (and 19 others), remove @bangle.io/editor-common (and 5 others), update nalanda to portal:/Users/kushanjoshi/code/nalanda
    
    Commit generated via `yarn stage`
    
    * refactors
    
    * fix extension tests
    
    * backlink e2e tests
    
    * fix e2e
    
    * fix ci
    
    * fix cal git file sha
    
    * fix
    
    * fix test
    
    * more tries
    
    * try more
    
    * improve effects run
    
    * fix e2e

--- lib/api/nsm/internal/internals.ts
+++ lib/api/nsm/internal/internals.ts
@@ -1,3 +1,4 @@
+import { getEternalVars } from '@bangle.io/bangle-store-context';
 import type { nsmExtensionRegistry } from '@bangle.io/extension-registry';
 import type {
   Dispatch,
@@ -11,26 +12,26 @@ import type { nsmPageSlice } from '@bangle.io/slice-page';
 import type { sliceRefreshWorkspace } from '@bangle.io/slice-refresh-workspace';
 import type { nsmUISlice } from '@bangle.io/slice-ui';
 
-import type { editorManagerProxy } from './nsm/editor';
+import { setEternalVars } from './eternal-vars';
 
 export type ApiSliceNames =
   | InferSliceNameFromSlice<typeof nsmUISlice>
   | InferSliceNameFromSlice<typeof nsmEditorManagerSlice>
   | InferSliceNameFromSlice<typeof nsmPageSlice>
   | InferSliceNameFromSlice<typeof nsmSliceWorkspace>
-  | InferSliceNameFromSlice<typeof editorManagerProxy>
   | InferSliceNameFromSlice<typeof nsmExtensionRegistry>
   | InferSliceNameFromSlice<typeof nsmNotificationSlice>
   | InferSliceNameFromSlice<typeof sliceRefreshWorkspace>;
 
-export type ApiStore = Store<ApiSliceNames>;
+export type ApiStore = Store;
 let _store: ApiStore;
 
 export function _internal_setStore(store: ApiStore) {
   _store = store;
+  setEternalVars(getEternalVars(store));
 }
 
-export function getStore(): ApiStore {
+export function _internal_getStore(): ApiStore {
   if (!_store) {
     throw new Error('Store not set');
   }
--- lib/editor-common/collapsible-heading-deco.ts
+++ lib/editor-common/collapsible-heading-deco.ts
@@ -8,8 +8,8 @@ import {
   Selection,
 } from '@bangle.dev/pm';
 
-import { intersectionObserverPluginKey } from '@bangle.io/editor-common';
-import { hasPluginStateChanged } from '@bangle.io/utils';
+import { intersectionObserverPluginKey } from './constants';
+import { hasPluginStateChanged } from './helpers';
 
 const name = 'collapsible_heading_deco';
 // TODO even after optimizations, this is far for perfect
@@ -105,10 +105,10 @@ function buildDeco(state: EditorState) {
       match.pos + 1,
       (view: EditorView) => {
         let wrapper = document.createElement('span');
-        wrapper.className = 'B-core-editor_deco-collapse-positioner';
+        wrapper.className = 'B-editor-common_deco-collapse-positioner';
         // wrapper.setAttribute('data-bangle-pos', match.pos + '');
         const child = document.createElement('span');
-        child.className = 'B-core-editor_deco-collapse';
+        child.className = 'B-editor-common_deco-collapse';
 
         wrapper.appendChild(child);
 
@@ -133,7 +133,7 @@ function buildDeco(state: EditorState) {
 
         child.appendChild(svg);
 
-        svg.setAttribute('class', 'B-core-editor_deco-collapse-child');
+        svg.setAttribute('class', 'B-editor-common_deco-collapse-child');
 
         svg.appendChild(
           getNode('path', {
