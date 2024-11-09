cf22927bdc549c93dd6a395cbaab3cd55f5746d3
Kushan Joshi <0o3ko0@gmail.com>
2024-11-09 00:55:44 -0500

    add e2e test sidebar (#18)

--- packages/ui/ui-components/src/app-sidebar.stories.tsx
+++ packages/ui/ui-components/src/app-sidebar.stories.tsx
@@ -69,8 +69,6 @@ const meta: Meta<typeof Sidebar> = {
 
 export default meta;
 
-type Story = StoryObj<typeof Sidebar>;
-
 type NavItem = {
   title: string;
   url: string;
@@ -78,56 +76,53 @@ type NavItem = {
   items?: NavItem[]; // Recursive type to allow nested navigation
 };
 
-export const SidebarExample: Story = {
-  render: () => {
-    const [input, updateInput] = React.useState<string | undefined>();
-    console.log({ input });
-    return (
-      <SidebarProvider
-        style={
-          {
-            '--BV-sidebar-width': '19rem',
-          } as React.CSSProperties
-        }
-      >
-        <AppSidebar
-          workspaces={data.teams}
-          tree={data.tree}
-          navItems={data.navMain}
-          searchValue={input}
-          onSearchValueChange={updateInput}
-        />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </header>
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-              <div className="aspect-video rounded-xl bg-muted/50" />
-              <div className="aspect-video rounded-xl bg-muted/50" />
-              <div className="aspect-video rounded-xl bg-muted/50" />
-            </div>
-            <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+export function AppSidebarExample() {
+  const [input, updateInput] = React.useState<string | undefined>();
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--BV-sidebar-width': '19rem',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar
+        workspaces={data.teams}
+        tree={data.tree}
+        navItems={data.navMain}
+        searchValue={input}
+        onSearchValueChange={updateInput}
+      />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
           </div>
-        </SidebarInset>
-      </SidebarProvider>
-    );
-  },
-};
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
 
 const data = {
   navMain: [
